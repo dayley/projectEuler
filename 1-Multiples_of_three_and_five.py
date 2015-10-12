b7faad5e1f6dccd7f90c90c7eabf9cd5b688d78a
Levi <mr@dayley.me>
2015-10-12 11:57:37 -0600

    version 0.1, remove PRINT() statements

--- 1-Multiples_of_three_and_five.py
+++ 1-Multiples_of_three_and_five.py
@@ -1,11 +1,9 @@
-__author__ = 'LD'
-
-'''
+"""
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000
-'''
+"""
 
 
 def threefives(origin, limit):
@@ -20,13 +18,10 @@ def threefives(origin, limit):
 def sieve(numlist):
     final = []
     for n in numlist:
-        if n % 3 is 0:
-            final.append(n)
-        elif n % 5 is 0:
+        if n % 3 is 0 or n % 5 is 0:
             final.append(n)
     return final
 
+
 def adder(final):
     return sum(final)
-
-print(adder(sieve(threefives(1, 1000))))
